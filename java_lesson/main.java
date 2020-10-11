22b72b468cc47d7183039c864ea90f7d4ce86c8d
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-10-11 09:42:47 +0900

    Javaの構造

--- java_lesson/main.java
+++ java_lesson/main.java
@@ -4,3 +4,10 @@ System.out.println("HOll World")
 System.out.println("HOll World")
 //()の中身を出力せよ、と言う命令
 //System.out.printlnは（）の中身を出力（表示）せよと言う命令
+
+//Javaの構造
+class Main{  //クラス部分
+  public static void main(string[]args){  //メソッド部分
+    //（ここにコードを書く）　処理部分
+  }//メソッドの処理の閉じ
+}//クラス部分の閉じ
