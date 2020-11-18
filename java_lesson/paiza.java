cf11494cd6ee54fdcb9b34961e41603190595016
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-11-19 00:34:10 +0900

    データ・変数の定義

--- java_lesson/paiza.java
+++ java_lesson/paiza.java
@@ -10,5 +10,15 @@ public class Main {
        System.out.println(10 / 4.0);
        System.out.println(10 % 4);
        System.out.println(10000 + 5000);
+       System.out.println(10000.1 + 5000.1);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(true); //true=真・fales=偽
+        int a;  //変数aの宣言
+        a = 10; //変数aに10を入れる
+        System.out.println(a); //変数aの値を表示
     }
 }
