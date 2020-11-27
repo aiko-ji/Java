134caf276f5a809d0e5fb5bdc542f00c557fadc4
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-11-27 18:31:43 +0900

    if分を試す

---
+++ java_lesson/paiza2.java
@@ -0,0 +1,11 @@
+//if文を試す
+public class Main {
+  public static void main(String[] args){
+      int score = 90;  //変数scoreに90を入れる
+      if (score >= 80) {
+          System.out.println("やったね！次もこの調子だ");
+          //int~調子だ"）;までもしも点数が80点以上なら設定した文字列が表示される。
+          //80点未満の時は何も表示されない
+      }
+  }
+}
