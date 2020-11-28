b465fb89d62c4cb848022dda0a94adb6ede183a1
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-11-28 23:12:57 +0900

    ランダムな段・九九

--- java_lesson/paiza2.java
+++ java_lesson/paiza2.java
@@ -45,3 +45,19 @@ public class Main {
       //変数iを含めたかけ算式。５x０〜５x９の10個のかけ算をする
   }
 }
+//ランダムに出す、九九の段
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+       Random rnd = new Random();  //ランダムな数を出す部品
+       int answer = rnd.nextInt(10);  //かける数
+       String question = "";  //問題文変数
+       for(int i = 0; i <= 9; i++) {
+           //問題文変数に、[ answer*i ]を足していきます
+           question = question + "[" + (answer * i) + "]";
+       }
+       System.out.println("何の段の九九でしょう？");
+       System.out.println(question);
+       System.out.println("答え:"+answer);
+    }
+}
