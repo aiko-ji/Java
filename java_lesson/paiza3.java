784e92b5608f60ca89463c027e1aaa780d79bdda
aiko-ji <68524938+aiko-ji@users.noreply.github.com>
2020-12-01 19:15:48 +0900

    おみくじメソッドでランダムに結果を出す

--- java_lesson/paiza3.java
+++ java_lesson/paiza3.java
@@ -68,3 +68,16 @@ public class Main {
         //計算問題を表示するメソッド
     }
 }
+//おみくじメソッドをランダムに作る
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        omikuji();  //おみくじメソッドを呼び出す
+    }
+    static void omikuji() {  //おみくじメソッド
+        String [] kuji = {"大吉","中吉","吉","末吉","凶"};
+        Random rnd = new Random();
+        int id = rnd.nextInt(kuji.length);
+        System.out.println(kuji[id]);
+    }
+}
